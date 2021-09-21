03a9ba5b4fd2f6680173e002449873b62dddba43
Kamenchukov <k.kamenchukov@gmail.com>
2021-09-21 21:49:56 +0300

    Add singleton

---
+++ VkApp/Singleton/MySession.swift
@@ -0,0 +1,17 @@
+//
+//  MySession.swift
+//  VkApp
+//
+//  Created by Константин Каменчуков on 21.09.2021.
+//
+
+import UIKit
+
+final class MySession {
+    static let shared = MySession()
+    
+    private init() {}
+    
+    var token: String?
+    var userId: Int?
+}
